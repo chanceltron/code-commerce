bb778842022b543df42528ae98374286763c1dfb
Chance Ludwick <chanceludwick@gmail.com>
2022-12-09 14:07:54 -0600

    init commit

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+import style from './styles/App.module.css';
+
+export default class App extends Component {
+  render() {
+    return <div></div>;
+  }
+}
